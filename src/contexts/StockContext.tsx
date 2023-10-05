ee667be05bd2128b4602478a9faca2acd34284cc
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-10-05 14:21:21 -0300

    fix: addItem and updateItem functions updated

--- src/contexts/StockContext.tsx
+++ src/contexts/StockContext.tsx
@@ -1,11 +1,19 @@
 import { ReactNode, createContext, useState } from "react";
-import { ItemIfc } from "../common/ItemIfc";
+import { ItemIfc, DefaultItemIfc } from "../common/interfaces";
 
 interface StockContextProviderProps {
   children: ReactNode
 }
 
-export const StockContext = createContext({})
+interface StockContextIfc {
+  items: ItemIfc[]
+  addItem: (newItem: DefaultItemIfc) => void
+  getItem: (id: number) => ItemIfc | undefined
+  updateItem: (id: number, newAttr: DefaultItemIfc) => void
+  deleteItem: (id: number) => void
+}
+
+export const StockContext = createContext({} as StockContextIfc)
 
 export const StockContextProvider = ({ children }: StockContextProviderProps ) => {
   const [ items, setItems ] = useState<ItemIfc[]>(() => {
@@ -23,7 +31,12 @@ export const StockContextProvider = ({ children }: StockContextProviderProps ) =
     return items
   })
 
-  const addItem = (item: ItemIfc) => {
+  const addItem = (newItem: DefaultItemIfc) => {
+    const id = Math.floor(Math.random() * 1000000)
+    const createdAt = new Date()
+    const updatedAt = new Date()
+    const item: ItemIfc = { id, createdAt, updatedAt, ...newItem}
+
     setItems( currentState => {
       const newState = [ ...currentState, item ]
       localStorage.setItem( 'stock-items', JSON.stringify(newState) )
@@ -33,13 +46,13 @@ export const StockContextProvider = ({ children }: StockContextProviderProps ) =
 
   const getItem = (id: number) => items.find(item => item.id === id)
 
-  const updateItem = ({ id, name, qtd, price, category, description }: { id: number, name: string, qtd: number, price: number, category: string, description: string }) => {
+  const updateItem = (id: number, newAttr: DefaultItemIfc) => {
     const updatedAt = new Date()
-
+    
     setItems( currentState => {
       const newState = currentState.map( item => {
         if (item.id === id) {
-          return { ...item, name, qtd, price, category, description, updatedAt }
+          return { ...item, ...newAttr, updatedAt }
         }
         return item
       })
